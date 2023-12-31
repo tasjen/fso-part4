f34b3da1fd1cf7b4ba15a6d1cbd2215576d0eaa3
tastas000 <TiiLg0o000@gmail.com>
2023-12-31 07:02:06 +0700

    finish 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,11 +1,20 @@
 const dummy = (blogs) => {
   return 1;
-}
+};
 
 const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
-}
+};
+
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? null
+    : blogs.reduce((mostFav, blog) =>
+        blog.likes > mostFav.likes ? blog : mostFav
+      );
+};
 module.exports = {
   dummy,
-  totalLikes
-}
+  totalLikes,
+  favoriteBlog,
+};
