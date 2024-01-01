d1171acefecc703bfac3bbb19b70e354cab29515
tastas000 <TiiLg0o000@gmail.com>
2024-01-02 06:00:02 +0700

    finish 4.18

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require('express-async-errors');
 const app = express();
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 
 
@@ -17,6 +18,7 @@ app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
